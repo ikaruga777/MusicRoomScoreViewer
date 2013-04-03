2f22181cc123dba070b4bb3095f4f835476885a9
ikaruga <ikaruga777@gmail.com>
2013-04-03 21:53:22 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MusicRoomScoreViewer::Application.config.secret_token = '3e84bc2323e4c9bf53e25d3bfa48de13495fec87907568c91c7e5fec3f1d832d3be163b6cec95bfef720d0b1542863c0137ba84ca650a3b9941b97cadccf1cae'
